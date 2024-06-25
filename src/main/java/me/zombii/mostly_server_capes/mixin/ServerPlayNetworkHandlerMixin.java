9a933a2a7c4612d2361ae244b8cdcc302c364319
Mr-Zombii <thezombiigames@gmail.com>
2024-06-25 15:20:06 -0500

    remove useless signing

--- src/main/java/me/zombii/mostly_server_capes/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/me/zombii/mostly_server_capes/mixin/ServerPlayNetworkHandlerMixin.java
@@ -102,17 +102,7 @@ public abstract class ServerPlayNetworkHandlerMixin extends ServerCommonNetworkH
         textures.add("signatureRequired", new JsonPrimitive(true));
 
         String newTextures = Base64.getEncoder().encodeToString(textures.toString().getBytes());
-        Property newTexturesProperty;
-        try {
-            Signature s = Signature.getInstance("SHA1withRSA");
-            PrivateKey key = NetworkEncryptionUtils.decodeRsaPrivateKeyPem("-----BEGIN RSA PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCXWH07BqcvG7zOXIU27RD+SWoA\ngSsbjwal4JXfMPJtJ0FMkZT60i3AGvpLx/3Z2hWxFC+rjJ/YyU7jwZ4UecxJXMByDCWhOLC9oFs8\n+jXefpnhJQnL1R4J42FBDOP+k4gLyoWiTVFhDnAeWJe+AHVPZoYpQiy4O6OxAnajWggUu7RMEe6/\n1U4pkainqO4IbwIU9HSI0Aj2CJaxwqplEZ2Ndk0FVAbjT+gTKU7jzTmAyucWvKgq+J9zzbqGox7e\nFdqa8lKeix1qZgg5HEX5+1U0zjo/j9lYqwpd2Dx6ltP8HX5233d1o1CCCiQiUxbQwpwBD7a1Kbes\ns8nVwHtHNUExAgMBAAECggEADNjDEI6ZBGOrXCz4Vdg7uAoNSGuueBFk9BGjRyXFfkR1w4UcC0pp\n/cOMeYBJaQOdEcHv5fTy4Jj97FFUFNLd52BLKaMkUpIYVpBVDlEC35cJgtX9BeNaQGafq8DnXYWy\nZ6XSQBumC8II/FpyzvsE+i9utfPbDexqEa1u2qDazL+ct8Lp+v/1N2v0gxXCtgangwQm+WhrMb33\nbG6Hb/+7SyyXZiDwuZQGb+NH7xAlUWNobwV6mOianm9Edpu7VdiKYNqckOA1KssjAU2XcfEjLKOK\nlrOVrmWuBf0zBghbyX1DLzSoIo6l6/ZuzWFlkskQ2WOrTCuNy/DeOvmrPFyaPQKBgQC+TCgP79P8\nBMbV8peDm/GpNmze7f+wufB7+E13lJi9qOkBVwpdvKrhHEle5kMTHVTyb/ld/2QB/GMjA4QOySml\n/HyI7yBrgzrtmrnJDBlSEKqLR9dLASjQ1277/2zLRNTX3C66+5HQf8bb1qy79BZ7wVQMYrTt/Bij\nHnNtuDbg0wKBgQDLmX5iJx7mlNbKd9nhh3CHgEDmkjyBjUNaoCZgBqyKKjqezYTBSWZlQoq4PxDr\njXEtjmxVJpnt0A2MJsDpch2SqY0NxRoRnv9lVQzQPm8lKLVSLvsgfzS14b9OOtvnOLso0IoE5lwH\nFy9SPnq7TtxHKNaN76ljahNMFxHdGY/zawKBgAKu2DHBU9/NwW/qx7AXVsTn+4j5Gg3H0VguHAl/\nkte5te9K5t6DdnVODMrFvFRcqYHxijaFD0fn4w3vUsFSnL+2W5sio/ZgF0iaUdw/y2uYyI7GKIbq\nMUUghHQRGduT0NxqQk5olZm283rOAAl2W4rLIwA5tAtW7pH+L+pwGnX5AoGAJ2xXToYos5lKZfYD\nGGpzXal156+VS7igvCGajKl+K1q18x30gMDHtP/HgJmlmxbOPXYAgBRzZdsNZH/0hr9z/nwaNfpe\na98PsP1g98m/F8DkKz+xnL9E45sMwgcfoYwYoXSP5Rb95tmUbiZu+WnxuU2tDdrEP93AUtF8wgU7\nHHUCgYB0qXO+BRcqG5QrGs5zBj8r/hR2uwawx1+IzDO07AlQzsTcARfPHo/i2XucNfjDqSz3QnI5\nToXMvISxRO/HjdTnN9Dtzr7wfvD0eEJhuLCY8/hYgfDHrnm9nwViX+yGZRFFG2qtMlkuYdzEXala\n0JPdaUiMF88VXG83DrWHdqkJYg==\n-----END RSA PRIVATE KEY-----");
-            System.out.println(key.getEncoded().length);
-            s.initSign(key);
-            s.update(newTextures.getBytes());
-            newTexturesProperty = new Property("textures", newTextures, Base64.getEncoder().encodeToString(s.sign()));
-        } catch (NoSuchAlgorithmException | SignatureException | InvalidKeyException | NetworkEncryptionException e) {
-            throw new RuntimeException(e);
-        }
+        Property newTexturesProperty = new Property("textures", newTextures);
 
         gameProfile.getProperties().removeAll("textures");
         gameProfile.getProperties().put("textures", newTexturesProperty);
